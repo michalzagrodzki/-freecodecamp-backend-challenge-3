2e42398f74d6157eb78b368c22693282c4d7f0f5
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-21 15:00:47 +0200

    Exercise 6.

--- myApp.js
+++ myApp.js
@@ -60,7 +60,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, function (err, data) {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
