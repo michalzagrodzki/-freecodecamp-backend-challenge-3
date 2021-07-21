4fc976c2e2787a9d15abb506382d46a1f656c836
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-21 16:35:23 +0200

    Exercise 9.

--- myApp.js
+++ myApp.js
@@ -87,8 +87,17 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    {
+      new: true,
+    },
+    function (err, data) {
+      if (err) return done(err);
+      done(null, data);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
